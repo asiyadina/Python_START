aa1d7a116b95ebd2485eed58c96e2b2e40bf3642
«Smirnova <>
2022-12-17 18:23:33 +0300

    Решение задач

--- HW_6/task_2.py
+++ HW_6/task_2.py
@@ -13,3 +13,35 @@
 [1, 3, 5]
 [1, 2, 5, 3, 10]
 """
+
+# Представлен список чисел.
+# Определить элементы списка, не имеющие повторений
+list1 = [9, 1, 5, 9, 4, 2, 7, 2, 9, 5, 3]
+list2 = [el for el in list1 if list1.count(el) == 1]
+print(list2)
+
+#список повторяемых чисел
+#myList = [9, 1, 5, 9, 4, 2, 7, 2, 9, 5, 3] 
+#occurrences = [] 
+
+#for item in myList : 
+#    count = 0 
+#    for x in myList : 
+#        if x == item :
+#             count += 1
+#    occurrences.append(count) 
+
+#duplicates = set() 
+#index = 0 
+#while index < len(myList) : 
+#    if occurrences[index] != 1 : 
+#        duplicates.add(myList[index]) 
+#    index += 1 
+
+#print(duplicates)
+
+
+#список без повторений
+#myList = [9, 1, 5, 9, 4, 2, 7, 2, 9, 5, 3]
+#mySet = set(myList) 
+#print(mySet)
